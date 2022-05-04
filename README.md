9c2e53b51152878fbf1765af175ccaba621bee4d
Rhiannon MacCreadie <rhiannonmaccreadie@gmail.com>
2022-05-05 01:36:09 +1200

    Form Layouts Final
    Commented with a README file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Form Layouts
- 
+
+Form Layouts is a homework example of using GUI to visualize forms and calenders.
+
+Please use the examples as you like <3
--- form_layouts.py
+++ form_layouts.py
@@ -1,7 +1,7 @@
 # form_layouts.py
 # Rhiannon MacCreadie
 # 04/05/2022
-# 
+# Practice making form layouts for class
 
 # Intialising
 import sys
@@ -14,39 +14,49 @@ window.setWindowTitle('QForm')
 window.setGeometry(500, 200, 600, 300)
 
 window.setCentralWidget(QtWidgets.QWidget())
-
-# Create Widget
-
-
+# Define Layouts
+# Main Layout
 h_layout = QtWidgets.QHBoxLayout()
+# Sub Layout
 form_layout = QtWidgets.QFormLayout()
 right_layout = QtWidgets.QVBoxLayout()
+
+# Set main layout
 window.centralWidget().setLayout(h_layout)
 
+# Define Widgets
 right_widget = QtWidgets.QWidget()
 form_widget = QtWidgets.QWidget()
 
+# Set Sub Layouts
 right_widget.setLayout(right_layout)
 form_widget.setLayout(form_layout)
 
+# Left Side Widgets
+# Create a form
+# String Answers
 form_layout.addRow("First Name", QtWidgets.QLineEdit())
 form_layout.addRow("Last Name", QtWidgets.QLineEdit())
 form_layout.addRow("Address", QtWidgets.QLineEdit())
 form_layout.addRow("City", QtWidgets.QLineEdit())
+# Checkbox Answer
 form_layout.addRow("Child", QtWidgets.QCheckBox())
 
+# Add widget to Main widget
 h_layout.addWidget(form_widget)
 
-
+# Right Side Widgets
+# Top Calender
 departure_date_calendar = QtWidgets.QCalendarWidget()
 right_layout.addWidget(departure_date_calendar)
+# Bottom Calender
 arrival_date_calendar = QtWidgets.QCalendarWidget()
 right_layout.addWidget(arrival_date_calendar)
 
+# Add right side widgets to main widget
 h_layout.addWidget(right_widget)
 
-window.setLayout(h_layout)
-
+# Output Code
 window.show()
 
 app.exec_()
